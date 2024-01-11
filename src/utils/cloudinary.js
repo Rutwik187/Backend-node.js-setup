d9e2ff27692ba6c6facb32f870ea60bd85d4c9b4
Rutwik187 <rutwikshinde34@gmail.com>
2024-01-11 20:08:35 +0530

    configered multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+          
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key:process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // uploading on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:'image',
+        })
+        //if file is uploaded successfully
+        console.log("file is uploaded on cloudinary successfully ", response.url)
+
+        return response
+    }catch (error){
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as upload file failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
